212e546fde30923e61ca02a0010cb646a77ff138
Igor Alencar <igordanice@gmail.com>
2024-11-26 20:27:57 -0300

    small visual adjustment

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import vonage_connection as vc
 
 
 
-vc.send_message(g.send_text_prompt("What is RCS?"+". Limit replies up to 8000 characters. Use less if possible."),"5588988128333")
+vc.send_message(g.send_text_prompt("What is RCS?"+
+                                   ". Limit replies up to 8000 characters. Use less if possible."),
+                                   "5588988128333")
 
 
