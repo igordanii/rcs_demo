4be93070e01b9564f38cba78325759aafc450c91
Igor Alencar <igordanice@gmail>
2024-12-06 14:09:23 -0300

    updated tokens

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import gemini as g
 import vonage_connection as vc
 
-
-
-vc.send_message(g.send_text_prompt("What is RCS?"+
+vc.send_message(g.send_text_prompt("What tips do you have for me to manage Google Calendar?"+
                                    ". Limit replies up to 8000 characters. Use less if possible."),
                                    "5588988128333")
-
-
